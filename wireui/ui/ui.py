811e9d2c8580d678cc85cfe2748524bf5937f5ac
Tim Schlottmann <tschlottmann@gmail.com>
2020-05-26 21:04:01 +0200

    Save internal settings at exit (closes #3)

--- wireui/ui/ui.py
+++ wireui/ui/ui.py
@@ -1,3 +1,5 @@
+from atexit import register
+
 from .console import set_verbosity
 
 from .menus import start_message
@@ -11,6 +13,8 @@ _SETTINGS_PATH = "./settings.json"
 
 def run_ui():
   w = WireUI("./settings.json")
+  register(w.write_settings_to_file)
+  register(w.write_sites_to_file)
   set_verbosity(w.get_setting("verbosity"))
   start_message()
   entrypoint_menu(w)
