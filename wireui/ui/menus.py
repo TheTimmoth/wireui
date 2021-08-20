b9485ad045de77204804f0995d7ebfd077251426
Tim Schlottmann <coding@timsc.de>
2021-08-20 18:06:27 +0200

    UI rework

--- wireui/ui/menus.py
+++ wireui/ui/menus.py
@@ -1,57 +1,54 @@
+from . import strings
+
+from .console import clear_screen
+from .console import leave_menu
 from .console import print_error
 from .console import print_message
+from .console import write_header
 
 from .peers import add_peer
 from .peers import delete_peer
 from .peers import edit_peer_connections
-from .peers import list_peers
 from .peers import rekey_peer
 
 from .shared import create_wireguard_config
-from .shared import yes_no_menu
 
 from .sites import add_site
 from .sites import delete_site
 from .sites import get_site_name
-from .sites import list_sites
 
 from ..library import WireUI
 
 
-def start_message():
-  print_message(0, "wireui")
-  print_message(0, "Tool for creating and managing wireguard configs")
-  print_message(0, "Version 0.1.0a")
-  print_message(0, "(C) 2020 Tim Schlottmann")
-  print_message(0, "")
-
-
 def entrypoint_menu(w: WireUI):
   while True:
-    sites_count = list_sites(w)
-    # if sites_count:
-    #   print_message(0, "0: List sites")
+    write_header("Main menu")
+
+    sites_count = len(w.get_sites())
     print_message(0, "1: Create new site")
     if sites_count:
       print_message(0, "2: Edit existing site")
       print_message(0, "3: Delete existing site")
       print_message(0, "4: Recreate config files")
+    print_message(0, "a: Informations about this program")
     print_message(0, "q: Exit")
     choice = input("What do you want to do? ")
 
-    # if choice == "0" and sites_count:
-    #   list_sites(w)
     if choice == "1":
+      leave_menu()
       site_menu(w, add_site(w))
     elif choice == "2" and sites_count:
-      list_sites(w)
+      leave_menu()
       site_menu(w, get_site_name(w, should_exist=True))
     elif choice == "3" and sites_count:
-      list_sites(w)
+      leave_menu()
       delete_site(w)
     elif choice == "4" and sites_count:
-      list_sites(w)
+      leave_menu()
       create_wireguard_config(w, get_site_name(w, should_exist=True))
+    elif choice == "a":
+      leave_menu()
+      _about()
     elif choice == "q":
       _exit(0)
 
@@ -61,14 +58,13 @@ def entrypoint_menu(w: WireUI):
 def site_menu(w: WireUI, site_name: str):
   leave = False
   while not leave:
-    peers_count = list_peers(w, site_name)
+    write_header(f"Site \"{site_name}\"")
+    peers_count = len(w.get_peer_names(site_name))
 
     # TODO: Remove this hint when editing is possible
     print_message(0,
                   "For full editing of peers please use the sites.json file!")
 
-    # if peers_count:
-    #   print_message(0, "0: List peers")
     print_message(0, "1: Add peer")
     if peers_count:
       print_message(0, "2: Create new keys for a peer")
@@ -78,26 +74,35 @@ def site_menu(w: WireUI, site_name: str):
     print_message(0, "q: Exit")
     choice = input("What do you want to do? ")
 
-    # if choice == "0" and peers_count:
-    #   list_peers(w, site_name)
     if choice == "1":
       add_peer(w, site_name)
     elif choice == "2" and peers_count:
-      list_peers(w, site_name)
       rekey_peer(w, site_name)
     elif choice == "3" and peers_count:
-      list_peers(w, site_name)
       delete_peer(w, site_name)
     elif choice == "4" and peers_count:
       edit_peer_connections(w, site_name)
     elif choice == "b":
       leave = True
+      leave_menu()
     elif choice == "q":
       _exit(0)
 
     print_message(0, "")
 
 
+def _about():
+  write_header("About")
+  print_message(0, strings.name)
+  print_message(0, strings.description)
+  print_message(0, f"Version {strings.version}")
+  print_message(0, f"{strings.copyright} {strings.author}")
+  print_message(0, "")
+  input("Press ENTER to go back...")
+  leave_menu()
+
+
 def _exit(i):
+  clear_screen()
   print_message(0, "Bye")
   exit(i)
--- wireui/ui/sites.py
+++ wireui/ui/sites.py
@@ -1,13 +1,16 @@
 import ipaddress
 
+from .console import leave_menu
 from .console import print_message
 from .console import print_error
+from .console import print_list
+from .console import yes_no_menu
+from .console import write_header
 
 from .shared import create_wireguard_config
 from .shared import edit_connection_table
 from .shared import edit_string
 from .shared import get_new_peer_properties
-from .shared import yes_no_menu
 
 from ..library import ConnectionTable
 from ..library import Peer
@@ -17,29 +20,29 @@ from ..library import SiteDoesNotExistError
 from ..library import WireUI
 
 
-def list_sites(w: WireUI) -> int:
-  sites = w.get_sites()
-  if sites:
-    print_message(0, "The following sites do exist:")
-    for s in sites:
-      print_message(0, s)
-  else:
-    print_message(0, "There are currently no sites.")
-  return len(sites)
-
-
 def add_site(w: WireUI) -> str:
+  write_header("Add new site")
+
   site_name = get_site_name(w, should_exist=False)
 
   ip = []
-  allow_ipv4 = yes_no_menu("Do you want to use IPv4?")
+  ipv4_network = ""
+  ipv6_network = ""
+  while not ipv4_network and not ipv6_network:
+    allow_ipv4, ipv4_network = _get_ip_network(4)
+    allow_ipv6, ipv6_network = _get_ip_network(6)
+    if not ipv4_network and not ipv6_network:
+      write_header("Collecting IP informations")
+      input(
+        "You have to enter at least one ipv4 or ipv6 network!\nPress ENTER to continue..."
+      )
+      leave_menu()
   if allow_ipv4:
-    ip.append(_get_ip_network(4))
-  allow_ipv6 = yes_no_menu("Do you want to use IPv6?")
+    ip.append(ipv4_network)
   if allow_ipv6:
-    ip.append(_get_ip_network(6))
+    ip.append(ipv6_network)
 
-  dns = _get_dns(w)
+  dns = _get_dns(w, allow_ipv4, allow_ipv6)
 
   peer_names = _get_peer_names(w)
 
@@ -63,10 +66,13 @@ def add_site(w: WireUI) -> str:
 
   create_wireguard_config(w, site_name)
 
+  leave_menu()
+
   return site_name
 
 
 def delete_site(w: WireUI):
+  write_header("Delete a site")
   site_name = get_site_name(w, should_exist=True)
   if yes_no_menu("Do you really want to delete the site \"" + site_name +
                  "\"?"):
@@ -76,23 +82,40 @@ def delete_site(w: WireUI):
     except SiteDoesNotExistError as e:
       print_error(0, "Site does not exist. Doing nothing...")
       print_error(0, e)
+  leave_menu()
 
 
 def get_site_name(w: WireUI, should_exist: bool) -> str:
   while True:
+    write_header("Enter site name")
+    _list_sites(w)
     name = input("Please enter the name of the site: ")
     exist = w.site_exists(name)
     if (should_exist and exist) or (not should_exist and not exist):
+      leave_menu()
       return name
     elif should_exist and not exist:
       print_error(0, f"Error: {name} does not exist.")
     elif not should_exist and exist:
       print_error(0, f"Error: {name} does already exist.")
+    leave_menu()
 
 
-# TODO: is correct messages
-def _get_ip_network(ip_version: int = 4) -> str:
-  while True:
+def _list_sites(w: WireUI):
+  sites = w.get_sites()
+  if sites:
+    print_message(0, "The following sites do exist:")
+    print_list(sites)
+  else:
+    print_message(0, "There are currently no sites.")
+
+
+def _get_ip_network(ip_version: int = 4) -> tuple[bool, str]:
+  write_header(f"Collecting IPv{ip_version} informations")
+  allow_ip = yes_no_menu(f"Do you want to use IPv{ip_version}?")
+  ip_network = ""
+  while allow_ip:
+    write_header()
     if ip_version == 4:
       s = "Please enter the IPv4 network the site should use.\n(For example \"10.0.0.0/24\"): "
     else:
@@ -105,42 +128,66 @@ def _get_ip_network(ip_version: int = 4) -> str:
     except ValueError as e:
       print_error(0, "Error: Input is not a valid IP network.")
       print_error(2, e)
-    else:
-      if ip_version != ip_network.version:
-        print_error(
-          0, "Error: You entered an IPv" + ip_network.version +
-          " network, but an IPv" + ip_version + " network is expected.")
-        continue
+      continue
+    if ip_version != ip_network.version:
+      print_error(
+        0, "Error: You entered an IPv" + ip_network.version +
+        " network, but an IPv" + ip_version + " network is expected.")
+      continue
+    ip_network = str(ip_network.with_prefixlen)
+    break
 
-      return str(ip_network.with_prefixlen)
+  leave_menu()
+
+  return (allow_ip, ip_network)
 
 
-def _get_dns(w: WireUI) -> list:
+def _get_dns(w: WireUI, allow_ipv4: bool, allow_ipv6: bool) -> list:
   while True:
+    write_header("Collecting DNS informations")
     dns = input(
       "Please enter the name of the dns servers (use ' ' as separation): ")
     dns = _convert_str_to_list(dns)
-    for a in dns:
-      try:
-        ipaddress.ip_address(a)
-      except ValueError as e:
-        dns.remove(a)
+    _check_dns(dns, allow_ipv4, allow_ipv6)
     print_message(0, "The following DNS entries have been detected:")
-    dns = _convert_list_to_str(dns)
 
     correct = False
-    while not correct:
+    while dns and not correct:
+      write_header()
+      print_list(dns)
       correct = yes_no_menu("Is everything correct?")
       if not correct:
+        dns = _convert_list_to_str(dns)
         dns = edit_string(w, dns)
         dns = _convert_str_to_list(dns)
-        print_message(0, "The following peers has been detected:")
-        dns = _convert_list_to_str(dns)
-    dns = _convert_str_to_list(dns)
-    return dns
+        _check_dns(dns, allow_ipv4, allow_ipv6)
+        print_message(0, "The following DNS entries have been detected:")
+      if not dns:
+        break
+    if dns:
+      break
+  leave_menu()
+  return dns
+
+
+def _check_dns(dns: list, allow_ipv4: bool, allow_ipv6: bool):
+  for a in dns:
+    try:
+      dns_ip_version = ipaddress.ip_address(a).version
+    except ValueError:
+      dns.remove(a)
+    else:
+      if (not allow_ipv4 and dns_ip_version == 4) or (not allow_ipv6
+                                                      and dns_ip_version == 6):
+        remove = yes_no_menu(
+          f"DNS Server {a} is IPv{dns_ip_version}, but IPv4 is not allowed.\nThis may not work correctly.\nShould the server be removed?"
+        )
+        if remove:
+          dns.remove(a)
 
 
 def _get_peer_names(w: WireUI) -> tuple:
+  write_header("Collecing peer names")
   # Get peer names
   peer_names = input(
     "Please enter the name of the peers (use ' ' as separation): ")
@@ -148,18 +195,18 @@ def _get_peer_names(w: WireUI) -> tuple:
 
   # Check names
   print_message(0, "The following peers has been detected:")
-  peer_names = _convert_list_to_str(peer_names)
 
   correct = False
   while not correct:
+    write_header()
+    print_list(peer_names)
     correct = yes_no_menu("Is everything correct?")
     if not correct:
+      peer_names = _convert_list_to_str(peer_names)
       peer_names = edit_string(w, peer_names)
       peer_names = _convert_str_to_list(peer_names)
       print_message(0, "The following peers has been detected:")
-      peer_names = _convert_list_to_str(peer_names)
-
-  peer_names = _convert_str_to_list(peer_names)
+  leave_menu()
   return peer_names
 
 
@@ -178,10 +225,8 @@ def _convert_list_to_str(l: list) -> str:
 
   Elements are separated with ' '"""
 
-  print_message(0, "The following peers has been detected:")
   s = ""
   for e in l:
-    print_message(0, str(e))
     s += f"{e} "
   s = s[:-1]
 
